64ca9551a7a4e7d866e4e55430196382748618ad
matheus souza <ruivorobado@gmail.com>
2021-09-14 10:28:37 -0300

    primeiro hook a ser testado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState, useEfect } from 'react'
 
 function App() {
-  const [repositories, setRepositories ] = useState([])
- 
+  const [repositories, setRepositories ] = useState([
+    {id: 1, name: 'repo-1'},
+    {id: 2, name: 'repo-2'},
+    {id: 3, name: 'repo-3'},
+  ])
+
  
   return (
-  <h1>oi</h1>
+    <ul>
+      {
+        repositories.map(repo => <li key={repo.id}> {repo.name} </li>)
+      }
+    </ul>
   );
 }
 
